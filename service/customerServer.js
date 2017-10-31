142205b8ffe238891f3dc0b1803a59803b4010b7
yongchao71 <yongchao71@163.com>
2017-10-31 18:50:16 +0800

    add encodeuri

--- service/customerServer.js
+++ service/customerServer.js
@@ -6,14 +6,15 @@ var logingServer = require('../security/loginserver');
 
 var defualtCfg={
     url:CONSTANT.remoteHost+":"+CONSTANT.remotePort+'/api/customer/',
-    contentType:'application/json'
+    contentType:'application/json;charset=UTF-8'
 };
 
 function clist(req, res, next){
     defualtCfg.method="GET";
     var opt=appUtil.extend({},defualtCfg);
-    opt.url+=`list?pageNo=${req.body.pageNo}&pageSize=${req.body.pageSize}&startDate=${req.body.startDate}&endDate=${req.body.endDate}&fieldValue=${req.body.fieldValue}&searchField=${req.body.searchField}`;
-   //console.log(opt.url);
+
+    opt.url+=`list?pageNo=${req.body.pageNo}&pageSize=${req.body.pageSize}&startDate=${req.body.startDate}&endDate=${req.body.endDate}&fieldValue=${encodeURI(req.body.fieldValue)}&searchField=${req.body.searchField}`;
+   console.log(opt.url);
     opt.callBack=function(error, response, body){
         if(error)
         {
