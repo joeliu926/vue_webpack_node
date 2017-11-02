c155a4bdaab42150150e9ca09ebd87574bc0cd55
yongchao71 <yongchao71@163.com>
2017-11-02 13:01:29 +0800

    add consult service

---
+++ service/consultsServer.js
@@ -0,0 +1,32 @@
+var CONSTANT=require('../config/constant');
+var httpClient=require('../utils/httpClient');
+var appUtil=require('../utils/appUtils');
+var logingServer = require('../security/loginserver');
+
+
+var defualtCfg={
+    url:CONSTANT.remoteHost+":"+CONSTANT.remotePort+'/api/consultRecord/',
+    contentType:'application/json;charset=UTF-8'
+};
+
+function getrecord(req, res, next){
+    defualtCfg.method="GET";
+    var opt=appUtil.extend({},defualtCfg);
+
+    opt.url+=`getRecords/${req.body.id}`;
+    console.log(opt.url);
+    opt.callBack=function(error, response, body){
+        if(error)
+        {
+            res.send(error);
+        }
+        else {
+            res.send(JSON.parse(body));
+        }
+    }
+    httpClient(opt);
+}
+
+module.exports = {
+    getrecord: getrecord,
+}
