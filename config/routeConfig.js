172145f007d4d2d9ecdbaa6880b428d37a49be7b
liuzhao <liuzhao@rongcapital.cn>
2017-12-06 17:32:11 +0800

    1732

--- config/routeConfig.js
+++ config/routeConfig.js
@@ -18,15 +18,13 @@ function registor(app) {
             require: '../routes/consults.js'
         },
         {
-
             root:"/case_base",
             require: '../routes/case_base.js'
-<<<<<<< HEAD
         },
         {
             root:"/triage",
             require: '../routes/triage.js'
-=======
+
         },{
             root:"/faceDiagnose",//faceDiagnose
             require: '../routes/faceDiagnose.js'
@@ -34,8 +32,6 @@ function registor(app) {
         },{
             root:"/product",
             require: '../routes/product.js'
-
->>>>>>> 1e567eb6c0bac5325e257ef483b2abc64043744f
         }
     ];
 
